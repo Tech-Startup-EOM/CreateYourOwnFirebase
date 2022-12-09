b7a44a08d045b6189844b744951ee9bda2cd7b56
Khalil-Burns <52607127+Khalil-Burns@users.noreply.github.com>
2022-12-09 11:23:12 -0500

    Update UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,47 +7,15 @@ const firestore = firebase.firestore();
 
 
 
-const register = async (req, res, next) => {
-    try {
-        await firebase.auth().createUser({
-            email: req.body.email,
-            password: req.body.password,
-            emailVerified: false,
-            disabled: false,
-            displayName: req.body.username
-        });
-    }
-    catch (error) {
-        console.log(error.message);
-        //res.status(400).send(error.message);
-    }
-};
-const login = async (req, res, next) => {
-    try {
-        /*await firebase.auth().signInWithEmailAndPassword({
-            email: req.body.email,
-            password: req.body.password
-        });
-        console.log('yess');*/
-        const test = await firebase.auth().getUserByEmail(req.body.email);
-            /*.then((user) => {
-                currentUser = user.toJSON();
-            }).catch(err => {
-                console.log('Email or password invalid!');
-            });*/
-        console.log(test);
-    }
-    catch (error) {
-        console.log(error.message);
-        //res.status(400).send(error.message);
-    }
-};
-
 const addUser = async (req, res, next) => {
     try {
-        const data = req.body;
-        await firestore.collection('users').doc().set(data);
-        //res.send('Record saved successfully');
+        /*
+        * print req, then try printing req.body to see the format
+        */
+        
+        /*
+        *  create a document in firebase using your firestor 'user' collection (I don't expect you to know the command. Here it isawait firestore.collection(/* the collection that you are creating the document in */).doc().set(/* the body of the request */);)
+        */
     }
     catch (error) {
         console.log(error.message);
@@ -55,116 +23,7 @@ const addUser = async (req, res, next) => {
     }
 };
 
-const getAllUsers = async (req, res, next) => {
-    try {
-        const users = await firestore.collection('users');
-        const data = await users.get();
-        const usersArray = [];
-        if (data.empty) {
-            //res.status(404).send('No user record found');
-            return;
-        }
-        data.forEach(doc => {
-            const user = new User(
-                doc.id,
-                doc.data().username,
-                doc.data().email,
-                doc.data().password
-            );
-            usersArray.push(user);
-        });
-    }
-    catch (error) {
-        //res.status(400).send(error.message);
-    }
-};
-
-const getUser = async (req, res, next) => {
-    try {
-        const data = req.body;
-        var output = [false, 'Email not found!'];
-        const user = await firestore.collection('users').where("email", "==", data.email)
-            .get()
-            .then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    if (doc.data().password == data.password) {
-                        output = [true, doc.data()];
-                    }
-                    else {
-                        output = [false, 'Incorrect Password'];
-                    }
-                });
-            });
-        return(output);
-    }
-    catch (error) {
-        //res.status(400).send(error.message);
-    }
-};
-
-const updateUser = async (req, res, next) => {
-    try {
-        const data = req.body;
-        console.log(data);
-        var output = [false, 'Email not found!'];
-        const user = await firestore.collection('users').where("email", "==", data.email)
-            .get()
-            .then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    if (doc.data().password == data.oldPassword) {
-                        doc.ref.update(
-                            {
-                                'email': data.email,
-                                'password': data.newPassword,
-                                'username': data.newUsername
-                            }
-                        );
-                        output = [true];
-                    }
-                    else {
-                        output = [false, 'Incorrect Password'];
-                    }
-                });
-            });
-        
-        return(output);
-    }
-    catch (error) {
-        console.log(error);
-        return([false, 'it don\'t work dude. Error: ', error]);
-    }
-}
-
-const deleteUser = async (req, res, next) => {
-    try {
-        const data = req.body;
-        var output = [false, 'Email not found!'];
-        const user = await firestore.collection('users').where("email", "==", data.email)
-            .get()
-            .then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    if (doc.data().password == data.password) {
-                        doc.ref.delete();
-                        output = [true];
-                    }
-                    else {
-                        output = [false, 'Incorrect Password'];
-                    }
-                });
-            });
-        return(output);
-    }
-    catch (error) {
-        //res.status(400).send(error.message);
-    }
-}
 
 module.exports = {
-    register,
-    login,
-    addUser,
-    getAllUsers,
-    getUser,
-    updateUser,
-    deleteUser
+    addUser
 }
